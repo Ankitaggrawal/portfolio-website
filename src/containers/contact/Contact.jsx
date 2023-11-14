8c0bf9def26288e2d7fb27616f684f21bedbd30f
Ankitaggrawal <akaggrawal4252@gmail.com>
2023-11-14 17:36:11 +0530

    .continued

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import "./contact.css";
+import email from '../../assets/email.png'
 
 const Contact = () => {
   return (
     <div className="po__contact">
         <div className="po__contact-text">
-            Let's Work Together
+            <p>LET<span>'</span>S W<span>O</span>RK T<span>O</span>GETHER</p>
         </div>
         <div className="po__contact-links">
-            <div className="po__emain-links__email">
-
+            <div className="po__contact-links__email">
+                <a href="mailto:akaggrawal4252@gmail.com">
+                <img src={email} alt="email" className='email'/>
+                <p>akaggrawal4252.gmail.com</p>
+                </a>
             </div>
             <div className="po__emain-links__social">
-                
+
             </div>
         </div>
     </div>
