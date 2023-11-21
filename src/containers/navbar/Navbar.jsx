f69474d60c3fa1282fdb49fab435c857d2ad8abe
Ankitaggrawal <akaggrawal4252@gmail.com>
2023-11-22 04:54:49 +0530

    Responsive Navbar

--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './navbar.css'
 import github from "../../assets/github.png"
 import linkedIn from "../../assets/linkedIn.png"
+import home from "../../assets/navbarIcon/home.png"
+import about from "../../assets/navbarIcon/about.png"
+import projects from "../../assets/navbarIcon/projects.png"
+import contact from "../../assets/navbarIcon/contacts.png"
+
 
 const Navbar = () => {
   return (
@@ -12,6 +17,15 @@ const Navbar = () => {
               <p><a href="#about">About</a></p>
               <p><a href="#projects">Work</a></p>
               <p><a href="#contact">Contact</a></p>
+            </div>
+
+            <div className="po__navbar-links__mobileComponents">
+              <a href="#home"><img src={ home } alt="H" /></a>
+              <a href="#about"><img src={ about } alt="A" /></a>
+              <a href="#projects"><img src={ projects } alt="P" /></a>
+              <a href="#contact"><img src={ contact } alt="C" /></a>
+
+
             </div>
           
             <div className="po__navbar-links__ids">
