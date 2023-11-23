5296fae390ae9994389c06422e4198052a45dc34
Ankitaggrawal <akaggrawal4252@gmail.com>
2023-11-23 06:28:52 +0530

    main mobile responsive

--- src/containers/main/Main.jsx
+++ src/containers/main/Main.jsx
@@ -21,6 +21,22 @@ const main = () => {
         </div>
       </div>
 
+      <div className="po__main-textMobile">
+        <div className="po__main-headingMobile">
+          <div>Hi, I’m </div>
+          <div className="aj">Ankit Kumar Aggrawal</div>
+        </div>
+        <p className="po__main-subheadingMobile">
+          A front-end web Developer and UI/UX designer
+        </p>
+        <div className="po__main-buttonsMobile">
+          <button>See my Resume</button>
+          <button>
+          <a href="#contact">Get in touch</a>
+          </button>
+        </div>
+      </div>
+
       <div className="po__main-illus">
         <img src={vector} alt="vector" />
       </div>
